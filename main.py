8a366f8f6f893d8e665f3349b7fb26783e6ecb03
Botato <51982229+Botato300@users.noreply.github.com>
2022-12-19 23:12:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ bot.remove_command('help')
 bot.add_cog(music_cog(bot))
 bot.add_cog(help_cog(bot))
 
-bot.run('ODU5OTk3OTg0NTA1NDYyNzk4.G569RW.8HKkEblmrLgXCEZwtQa0msowhH-EDMLQ0xBflQ')
+bot.run('YOUR_TOKEN')
